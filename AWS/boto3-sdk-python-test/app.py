4fcf5cc0b801b29fcfd089374bf12b0a54b6a65c
AbhayKumarVishwakarma <3492akv@gmail.com>
2026-03-20 06:54:30 -0400

    testing boto2 aws

--- AWS/boto3-sdk-python-test/app.py
+++ AWS/boto3-sdk-python-test/app.py
@@ -1,6 +1,14 @@
 import boto3
+import os
 
-s3 = boto3.resource("s3")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
+SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
+
+s3 = boto3.resource("s3", aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
 for bucket in s3.buckets.all():
     print(bucket.name)
