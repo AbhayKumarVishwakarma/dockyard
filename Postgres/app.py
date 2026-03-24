6ed4b318c290726d975800027986cae91c1207dc
AbhayKumarVishwakarma <3492akv@gmail.com>
2026-03-24 06:27:11 -0400

    feat: run and test postgres from docker container

---
+++ Postgres/app.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="abraj",
+    port=5432
+)
+
+cur = conn.cursor()
+
+# # Create a table
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS users (
+#         id SERIAL PRIMARY KEY,
+#         name VARCHAR(100),
+#         email VARCHAR(100) UNIQUE
+#     )
+# ''')
+# print("Table created successfully.")
+
+
+# # Insert a record
+# cur.execute(
+#     "INSERT INTO users (name, email) VALUES (%s, %s)",
+#     # ("John Doe", "john.doe@example.com"),
+#     ("Jane Smith", "jane.smith@example.com")
+# )
+# print("Records inserted successfully.")
+
+# conn.commit()
+
+
+# Query the table
+cur.execute('SELECT * FROM users')
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
